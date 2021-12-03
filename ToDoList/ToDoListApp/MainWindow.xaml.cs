f0f564171e13bd8b295b7dcd65f541d4a34c4e9c
Cuong-lee32 <88323905+Cuong-lee32@users.noreply.github.com>
2021-12-03 22:56:23 +0700

    Add files via upload

---
+++ ToDoList/ToDoListApp/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using ToDoListApp.Models;
+using ToDoListApp.Services;
+
+
+namespace ToDoListApp
+{
+    public partial class MainWindow : Window
+    {
+        #region Fields
+
+        private BindingList<ToDoModel> _toDoDataList;
+        private FileIoServices _fileIoServices;
+        private readonly string _path = $"{Environment.CurrentDirectory}\\todoDataList.json";
+
+        #endregion
+
+
+        #region Methods
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _fileIoServices = new FileIoServices(_path);
+
+            try
+            {
+                _toDoDataList = _fileIoServices.LoadData();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                Close();
+            }
+
+            DataGridToDoList.ItemsSource = _toDoDataList;
+            _toDoDataList.ListChanged += ToDoDataList_ListChanged;
+        }
+
+        private void ToDoDataList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            try
+            {
+                if (e.ListChangedType == ListChangedType.ItemDeleted ||
+                    e.ListChangedType == ListChangedType.ItemAdded ||
+                    e.ListChangedType == ListChangedType.ItemChanged)
+                {
+                    _fileIoServices.SaveData(sender);
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                Close();
+            }
+        }
+
+        #endregion
+
+        private void DataGridToDoList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ToDoList/ToDoListApp/Models/ToDoModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Newtonsoft.Json;
+
+
+namespace ToDoListApp.Models
+{
+    internal sealed class ToDoModel : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private bool _isDone;
+        private string _text;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+
+        #region Properties
+
+        [JsonProperty(PropertyName = "creationDate")]
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        [JsonProperty(PropertyName = "isDone")]
+        public bool IsDone
+        {
+            get => _isDone;
+            set
+            {
+                if (_isDone == value)
+                {
+                    return;
+                }
+                _isDone = value;
+                OnPropertyChanged(nameof(IsDone));
+            }
+        }
+
+        [JsonProperty(PropertyName = "text")]
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                if (_text == value)
+                {
+                    return;
+                }
+                _text = value;
+                OnPropertyChanged(nameof(Text));
+            }
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+    }
+}
