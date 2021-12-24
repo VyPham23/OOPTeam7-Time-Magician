ec43f0bc35edafe2f63adf413610c1f165d1016d
DavidTram0311 <90774831+DavidTram0311@users.noreply.github.com>
2021-12-24 14:34:26 +0700

    Add files via upload

---
+++ ToDoList/StartupBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ToDoList.Infastructure;
+
+namespace TodoList
+{
+    public class StartupBase
+    {
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddDbContext<ToDoContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ToDoContext")))
+        }
+    }
+}
